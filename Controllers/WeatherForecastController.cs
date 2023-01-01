732f6c1c4e32b4c42e06676dbbcdc6cb7169c017
Oleksandr Barylo <sbarylo20023@gmail.com>
2023-01-01 19:59:55 +0200

    Use DateTime

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -23,7 +23,7 @@ public class WeatherForecastController : ControllerBase
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            DateTime = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             WeatherSummary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
